331b26d96976ae8d2abd3b95958c42153b168ac0
smallboyintech <111036308+tabpaddy@users.noreply.github.com>
2024-05-26 23:13:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,179 @@
+<?php
+include('./partials/header.php');
+?>
+
+    <section class="featured">
+        <div class="container featured__container">
+            <div class="post__thumbnail">
+                <img src="./images/blog1.jpg">
+            </div>
+            <div class="post__info">
+            <a href="" class="category__button">Wild Life</a>
+            <h2 class="post_title"><a href="post.php">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</a></h2>
+            <p class="post_body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quaerat illo, aut doloribus sunt eos, labore alias ducimus, harum explicabo dolorum temporibus inventore ratione. Eveniet voluptatibus non distinctio, quo id nostrum libero accusamus esse velit rem.
+            </p>
+            <div class="post__author">
+                <div class="post__author-avatar">
+                    <img src="./images/avatar2.jpg" alt="ha">
+                </div>
+                <div class="post__author-info">
+                    <h5>By: Mary Dan</h5>
+                    <small>june 10, 2022 - 07:23</small>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end of feautred post -->
+
+    <section class="post">
+        <div class="container post__container">
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog2.jpg" alt="eer">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, numquam.</a></h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum delectus cupiditate vel quo totam ipsam quae provident! Temporibus, molestiae!
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar4.jpg" alt="ij">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: jane Mills</h5>
+                            <small>june 13, 2024 - 10:24</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog3.jpg" alt="eer">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, numquam.</a></h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum delectus cupiditate vel quo totam ipsam quae provident! Temporibus, molestiae!
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar5.jpg" alt="ij">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: ben Mills</h5>
+                            <small>june 13, 2024 - 10:24</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog4.jpg" alt="eer">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, numquam.</a></h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum delectus cupiditate vel quo totam ipsam quae provident! Temporibus, molestiae!
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar6.jpg" alt="ij">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: demola Mills</h5>
+                            <small>june 13, 2024 - 10:24</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog5.jpg" alt="eer">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, numquam.</a></h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum delectus cupiditate vel quo totam ipsam quae provident! Temporibus, molestiae!
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar7.jpg" alt="ij">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: femi tekena</h5>
+                            <small>june 13, 2024 - 10:24</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog6.jpg" alt="eer">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, numquam.</a></h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum delectus cupiditate vel quo totam ipsam quae provident! Temporibus, molestiae!
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar8.jpg" alt="ij">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: power dele</h5>
+                            <small>june 13, 2024 - 10:24</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post__thumbnail">
+                    <img src="./images/blog7.jpg" alt="eer">
+                </div>
+                <div class="post__info">
+                    <a href="" class="category__button">Wild Life</a>
+                    <h3 class="post__title"><a href="post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, numquam.</a></h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eum delectus cupiditate vel quo totam ipsam quae provident! Temporibus, molestiae!
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/avatar9.jpg" alt="ij">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: ifeoma badmus</h5>
+                            <small>june 13, 2024 - 10:24</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </section>
+
+    <!-- end of section -->
+
+    <section class="category__buttons">
+        <div class="container category__buttons-container">
+            <a href="category-post.php" class="category__button">Category</a>
+            <a href="category-post.php" class="category__button">Wild</a>
+            <a href="category-post.php" class="category__button">Travel</a>
+            <a href="category-post.php" class="category__button">Food</a>
+            <a href="category-post.php" class="category__button">Tech</a>
+            <a href="category-post.php" class="category__button">Crypto</a>
+        </div>
+    </section>
+
+    <!-- end of category button -->
+
+    <?php
+include('./partials/footer.php');
+?>
---
+++ post.php
@@ -0,0 +1,43 @@
+<?php
+include('./partials/header.php');
+?>
+
+    <section class="singlepost">
+        <div class="container singlepost__container">
+            <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
+            <div class="post__author">
+                <div class="post__author-avatar">
+                    <img src="./images/avatar2.jpg" alt="ha">
+                </div>
+                <div class="post__author-info">
+                    <h5>By: Mary Dan</h5>
+                    <small>june 10, 2022 - 07:23</small>
+                </div>
+            </div>
+            <div class="singlepost__thumbnail">
+                <img src="./images/blog14.jpg" alt="hssu">
+            </div>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum maiores consectetur doloribus. Modi debitis facilis eveniet, dolor, dolorum quia pariatur architecto libero nostrum, animi ratione dicta sapiente ipsum officia! Sapiente voluptas eveniet, iusto veritatis numquam non exercitationem ex odio nihil?
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum maiores consectetur doloribus. Modi debitis facilis eveniet, dolor, dolorum quia pariatur architecto libero nostrum, animi ratione dicta sapiente ipsum officia! Sapiente voluptas eveniet, iusto veritatis numquam non exercitationem ex odio nihil?
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum maiores consectetur doloribus. Modi debitis facilis eveniet, dolor, dolorum quia pariatur architecto libero nostrum, animi ratione dicta sapiente ipsum officia! Sapiente voluptas eveniet, iusto veritatis numquam non exercitationem ex odio nihil?
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum maiores consectetur doloribus. Modi debitis facilis eveniet, dolor, dolorum quia pariatur architecto libero nostrum, animi ratione dicta sapiente ipsum officia! Sapiente voluptas eveniet, iusto veritatis numquam non exercitationem ex odio nihil?
+            </p>
+        </div>
+    </section>
+
+    <!--end of single post -->
+
+ 
+
+
+
+    <?php
+include('./partials/footer.php');
+?>
