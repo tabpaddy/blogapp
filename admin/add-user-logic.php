940b9cfd3e1e5bd9916ee8ebf47552e7b1f5d66f
smallboyintech <111036308+tabpaddy@users.noreply.github.com>
2024-05-28 23:04:45 +0100

    Add files via upload

---
+++ admin/add-user-logic.php
@@ -0,0 +1,91 @@
+<?php
+require 'config/database.php';
+
+// get form data if sign  button is click.
+if(isset($_POST['submit'])){
+    $firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $lastname = filter_var($_POST['lastname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $username = filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $cpassword = filter_var($_POST['cpassword'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $userrole = filter_var($_POST['userrole'], FILTER_SANITIZE_NUMBER_INT);
+    $avatar = $_FILES['avatar'];
+
+    //validate input values
+    if(!$firstname){
+        $_SESSION['add-user'] = "Please enter your First Name";
+    }elseif(!$lastname){
+        $_SESSION['add-user'] = "Please enter your Last Name";
+    }elseif(!$username){
+        $_SESSION['add-user'] = "Please enter your Username";
+    }elseif(!$email){
+        $_SESSION['add-user'] = "Please enter your Email";
+    }elseif(strlen($password) < 8 || strlen($cpassword) < 8){
+        $_SESSION['add-user'] = "Password should be 8+ character";
+    }elseif(!$avatar['name']){
+        $_SESSION['add-user'] = "Please add avatar";
+    }else{
+        //check if password do not match
+        if($password !== $cpassword){
+            $_SESSION['add-user']="Password do not match";
+        }else{
+            // hash password
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            
+            //check if username or email already exit in the database
+            $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email'";
+            $user_check_result = mysqli_query($con, $user_check_query);
+            if(mysqli_num_rows($user_check_result) > 0){
+                $_SESSION['add-user'] = "Username or Email already exit";
+            }else{
+                //work on avatar
+                //rename avatar
+                $time = time();//make each image name unique using the timestamp
+                $avatar_name = $time . $avatar['name'];
+                $avatar_tmp_name = $avatar['tmp_name'];
+                $avatar_destination_path = '../image/'.$avatar_name;
+
+                // make sure file is an image
+                $allowed_files = ['png', 'jpg', 'jpeg'];
+                $extention = explode('.', $avatar_name);
+                $extention = end($extention);
+                if(in_array($extention, $allowed_files)){
+                    //make sure imge is not too large (1mb+)
+                    if($avatar['size'] < 1000000) {
+                        //upload avatar
+                        move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
+                    }else{
+                        $_SESSION['add-user'] = "File size too big. should be less than 1mb";
+                    }
+                }else{
+                    $_SESSION['add-user'] = "File should be png, jpg or jpeg";
+                }
+            }
+        }
+
+    }
+
+    //redirect back to add-user page if there was any problem
+    if(isset($_SESSION['add-user'])) {
+        //pass form data back to add-user page
+        $_SESSION['add-user-data'] = $_POST;
+        header('location: ' . ROOT_URL . 'admin/add-user.php');
+        die();
+    }else{
+        //insert new users into user table
+        $insert_user_query = "INSERT INTO users (firstname, lastname, username, email, password, avatar, is_admin) VALUES('$firstname','$lastname', '$username', '$email', '$hashed_password', '$avatar_name', $userrole)";
+        $insert_user_result= mysqli_query($con, $insert_user_query);
+        if(!mysqli_errno($con)){
+            // redirct to login page with success message
+            $_SESSION['add-user-success'] = "User $firstname $lastname Added Successful.";
+            header('location: ' . ROOT_URL . 'admin/manage-user.php');
+            die();
+        }
+    }
+
+}else{
+    //if button wasn't clicked bounce back to add-user page
+    header('location: ' . ROOT_URL . 'admin/add-user.php');
+    die();
+}
--- admin/add-user.php
+++ admin/add-user.php
@@ -1,29 +1,46 @@
 <?php
 include('./partials/header.php');
+//get back form data if there was an error
+$firstname = $_SESSION['add-user-data']['firstname'] ?? null;
+$lastname = $_SESSION['add-user-data']['lastname'] ?? null;
+$username = $_SESSION['add-user-data']['username'] ?? null;
+$email = $_SESSION['add-user-data']['email'] ?? null;
+$password = $_SESSION['add-user-data']['password'] ?? null;
+$cpassword = $_SESSION['add-user-data']['cpassword'] ?? null;
+
+
+
+//delete session data
+unset($_SESSION['add-user-data']);
 ?>
     
     <section class="form__section">
     <div class="container form__selection-container">
         <h2>Add User</h2>
-        <div class="alert__message error">
-            <p>this is a success message</p>
-        </div>
-        <form action="" enctype="multipart/form-data">
-            <input type="text" name="" id="" placeholder="First Name" required>
-            <input type="text" name="" id="" placeholder="Last Name" required>
-            <input type="text" name="" id="" placeholder="Username" required>
-            <input type="email" name="" id="" placeholder="Email" required>
-            <input type="password" name="" id="" placeholder="Create Password" required>
-            <input type="password" name="" id="" placeholder="Confirm Password" required>
-            <select name="" id="">
+        <?php if(isset($_SESSION['add-user'])): ?>
+            <div class="alert__message error">
+            <p>
+                <?= $_SESSION['add-user'];
+                unset($_SESSION['add-user']) ?>
+            </p>
+            </div>
+        <?php endif ?>
+        <form action="<?= ROOT_URL ?>admin/add-user-logic.php" enctype="multipart/form-data" method="post">
+            <input type="text" name="firstname" id="" placeholder="First Name" value="<?= $firstname ?>">
+            <input type="text" name="lastname" id="" placeholder="Last Name" value="<?= $lastname ?>">
+            <input type="text" name="username" id="" placeholder="Username" value="<?= $username ?>">
+            <input type="email" name="email" id="" placeholder="Email" value="<?= $email ?>">
+            <input type="password" name="password" id="" placeholder="Create Password" value="<?= $password ?>">
+            <input type="password" name="cpassword" id="" placeholder="Confirm Password" value="<?= $cpassword ?>">
+            <select name="userrole">
                 <option value="0">Author</option>
                 <option value="1">Admin</option>
             </select>
             <div class="form__control">
                 <label for="avatar">User Avatar</label>
-                <input type="file" id="avatar">
+                <input type="file" id="avatar" name="avatar" value="<?= $avatar ?>">
             </div>
-            <button type="submit" class="btn">Add User</button>
+            <button type="submit" class="btn" name="submit">Add User</button>
         </form>
     </div>
 </section>
