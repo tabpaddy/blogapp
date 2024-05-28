985f0ecbc5dca0ee4ef986bd49169766fed149aa
smallboyintech <111036308+tabpaddy@users.noreply.github.com>
2024-05-28 23:09:52 +0100

    Add files via upload

--- signup-logic.php
+++ signup-logic.php
@@ -1,5 +1,4 @@
 <?php
-
 require 'config/database.php';
 
 // get signup form data if sign p button is click.
@@ -20,8 +19,8 @@ if(isset($_POST['submit'])){
     }elseif(!$username){
         $_SESSION['signup'] = "Please enter your Username";
     }elseif(!$email){
-        $_SESSION['email'] = "Please enter your Email";
-    }elseif(strlen($password) < 8 || strlen($cpassword) > 8){
+        $_SESSION['signup'] = "Please enter your Email";
+    }elseif(strlen($password) < 8 || strlen($cpassword) < 8){
         $_SESSION['signup'] = "Password should be 8+ character";
     }elseif(!$avatar['name']){
         $_SESSION['signup'] = "Please add avatar";
@@ -32,9 +31,58 @@ if(isset($_POST['submit'])){
         }else{
             // hash password
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            
+            //check if username or email already exit in the database
+            $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email'";
+            $user_check_result = mysqli_query($con, $user_check_query);
+            if(mysqli_num_rows($user_check_result) > 0){
+                $_SESSION['signup'] = "Username or Email already exit";
+            }else{
+                //work on avatar
+                //rename avatar
+                $time = time();//make each image name unique using the timestamp
+                $avatar_name = $time . $avatar['name'];
+                $avatar_tmp_name = $avatar['tmp_name'];
+                $avatar_destination_path = 'image/'.$avatar_name;
+
+                // make sure file is an image
+                $allowed_files = ['png', 'jpg', 'jpeg'];
+                $extention = explode('.', $avatar_name);
+                $extention = end($extention);
+                if(in_array($extention, $allowed_files)){
+                    //make sure imge is not too large (1mb+)
+                    if($avatar['size'] < 1000000) {
+                        //upload avatar
+                        move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
+                    }else{
+                        $_SESSION['signup'] = "File size too big. should be less than 1mb";
+                    }
+                }else{
+                    $_SESSION['signup'] = "File should be png, jpg or jpeg";
+                }
+            }
         }
 
     }
+
+    //redirect back to signup page if there was any problem
+    if(isset($_SESSION['signup'])) {
+        //pass form data back to signup page
+        $_SESSION['signup-data'] = $_POST;
+        header('location: ' . ROOT_URL . 'signup.php');
+        die();
+    }else{
+        //insert new users into user table
+        $insert_user_query = "INSERT INTO users (firstname, lastname, username, email, password, avatar, is_admin) VALUES('$firstname','$lastname', '$username', '$email', '$hashed_password', '$avatar_name', 0)";
+        $insert_user_result= mysqli_query($con, $insert_user_query);
+        if(!mysqli_errno($con)){
+            // redirct to login page with success message
+            $_SESSION['signup-success'] = "Registration Successful. Please log in!";
+            header('location: ' . ROOT_URL . 'signing.php');
+            die();
+        }
+    }
+
 }else{
     //if button wasn't clicked bounce back to signup page
     header('location: ' . ROOT_URL . 'signup.php');
--- signup.php
+++ signup.php
@@ -1,5 +1,17 @@
 <?php
 include ('config/database.php');
+
+//get back form data if there was a registartion error
+$firstname = $_SESSION['signup-data']['firstname'] ?? null;
+$lastname = $_SESSION['signup-data']['lastname'] ?? null;
+$username = $_SESSION['signup-data']['username'] ?? null;
+$email = $_SESSION['signup-data']['email'] ?? null;
+$password = $_SESSION['signup-data']['password'] ?? null;
+$cpassword = $_SESSION['signup-data']['cpassword'] ?? null;
+$avatar = $_SESSION['signup-data']['avatar'] ?? null;
+
+//delete signup data session
+unset($_SESSION['signup-data']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +20,7 @@ include ('config/database.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Website</title>
     <!-- custom css -->
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>css/style.css">
     <!-- icon scout -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <!-- google fonts -->
@@ -22,19 +34,25 @@ include ('config/database.php');
     <section class="form__section">
     <div class="container form__selection-container">
         <h2>Sign Up</h2>
-        <div class="alert__message error">
-            <p>this is an error message</p>
-        </div>
+        <?php if(isset($_SESSION['signup'])): ?>
+                <div class="alert__message error">
+                    <p>
+                        <?= $_SESSION['signup'];
+                        unset($_SESSION['signup']);
+                        ?> 
+                    </p>
+                </div>  
+        <?php endif ?>
         <form action="<?=ROOT_URL?>signup-logic.php" enctype="multipart/form-data" method="post">
-            <input type="text" name="firstname" id="" placeholder="First Name" required>
-            <input type="text" name="lastname" id="" placeholder="Last Name" required>
-            <input type="text" name="username" id="" placeholder="Username" required>
-            <input type="email" name="email" id="" placeholder="Email" required>
-            <input type="password" name="password" id="" placeholder="Create Password" required>
-            <input type="password" name="cpassword" id="" placeholder="Confirm Password" required>
+            <input type="text" name="firstname" id="" placeholder="First Name" value="<?= $firstname ?>">
+            <input type="text" name="lastname" id="" placeholder="Last Name" value="<?= $lastname ?>">
+            <input type="text" name="username" id="" placeholder="Username" value="<?= $username ?>">
+            <input type="email" name="email" id="" placeholder="Email" value="<?= $email ?>">
+            <input type="password" name="password" id="" placeholder="Create Password" value="<?= $password ?>">
+            <input type="password" name="cpassword" id="" placeholder="Confirm Password" value="<?= $cpassword ?>">
             <div class="form__control">
                 <label for="avatar">Avatar</label>
-                <input type="file" name="avatar" id="avatar" required>
+                <input type="file" name="avatar" id="avatar" value="<?= $avatar ?>">
             </div>
             <button type="submit" class="btn" name="submit">Sign Up</button>
             <small>Already have an account? <a href="<?=ROOT_URL?>signing.php">Sign In</a></small>
