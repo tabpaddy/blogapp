613b626ca854ddf6e27f2b203adbce3769c2322c
smallboyintech <111036308+tabpaddy@users.noreply.github.com>
2024-05-28 23:05:25 +0100

    Add files via upload

--- admin/manage-user.php
+++ admin/manage-user.php
@@ -1,8 +1,23 @@
 <?php
 include('./partials/header.php');
+
+// fetch users from database but not current user
+$current_admin_id = $_SESSION['user-id'];
+
+$query = "SELECT * FROM users WHERE NOT id=$current_admin_id";
 ?>
 
 <section class="dashboard">
+<?php if(isset($_SESSION['add-user-success'])): ?>
+            <div class="alert__message success container">
+                <p>
+                    <?= 
+                    $_SESSION['add-user-success'];
+                    unset($_SESSION['add-user-success']);
+                    ?>
+                </p>
+        </div>
+        <?php endif ?>
     <div class="container dashboard__container">
         <button id="show__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-right-b"></i></button>
         <button id="hide__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-left-b"></i></button>
@@ -11,14 +26,14 @@ include('./partials/header.php');
                 <li><a href="add-post.php"><i class="uil uil-pen"></i>
                 <h5>Add Post</h5>
                 </a></li>
-                <li><a href="dashboard.php"  class="active"><i class="uil uil-postcard"></i>
+                <li><a href="index.php"><i class="uil uil-postcard"></i>
                 <h5>Manage Post</h5>
                 </a></li>
                 <?php if(isset($_SESSION['user_is_admin'])): ?>
                 <li><a href="add-user.php"><i class="uil uil-user-plus"></i>
                 <h5>Add User</h5>
                 </a></li>
-                <li><a href="manage-user.php"><i class="uil uil-users-alt"></i>
+                <li><a href="manage-user.php" class="active"><i class="uil uil-users-alt"></i>
                 <h5>Manage User</h5>
                 </a></li>
                 <li><a href="add-category.php"><i class="uil uil-edit"></i>
@@ -36,30 +51,27 @@ include('./partials/header.php');
             <table>
                 <thead>
                     <tr>
-                        <th>Title</th>
-                        <th>Category</th>
+                        <th>Name</th>
+                        <th>Username</th>
                         <th>Edit</th>
                         <th>Delete</th>
+                        <th>Admin</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Lorem ipsum dolor sit amet.</td>
-                        <td>Travel</td>
+                        <td>ernest achieve</td>
+                        <td>achieve</td>
                         <td><a href="edit-user.php" class="btn sm">Edit</a></td>
                         <td><a href="Delete-user.php" class="btn sm danger">Delete</a></td>
+                        <td>Yes</td>
                     </tr>
                     <tr>
-                        <td>Lorem ipsum dolor sit amet.</td>
-                        <td>food</td>
+                        <td>kingsley achieve</td>
+                        <td>kingsley</td>
                         <td><a href="edit-user.php" class="btn sm">Edit</a></td>
                         <td><a href="Delete-user.php" class="btn sm danger">Delete</a></td>
-                    </tr>
-                    <tr>
-                        <td>Lorem ipsum dolor sit amet.</td>
-                        <td>Music</td>
-                        <td><a href="edit-user.php" class="btn sm">Edit</a></td>
-                        <td><a href="Delete-user.php" class="btn sm danger">Delete</a></td>
+                        <td>No</td>
                     </tr>
                 </tbody>
             </table>
